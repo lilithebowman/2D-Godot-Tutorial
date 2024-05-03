c83a07633ad44392644e8f95a6fadd81c05b8fea
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-05-03 14:07:35 -0400

    Speedy Saucer Done

--- Speedy Saucer/Scenes/level.gd
+++ Speedy Saucer/Scenes/level.gd
@@ -3,3 +3,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Game initializing...")
+
+
+func _on_maze_body_exited(body):
+	print("Body exited")
+	get_tree().reload_current_scene()
+	pass
