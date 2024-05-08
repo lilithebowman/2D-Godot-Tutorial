50fcb3f5478e2d109113f31c0bc17c6fda59cabb
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-05-08 13:48:15 -0400

    Tweaks to the acceleration

--- Alien Attack/Scripts/rocket.gd
+++ Alien Attack/Scripts/rocket.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-@export var acceleration = 50
+@export var acceleration = 25
 @export var speed = 300
-var current_speed = 0
+var current_speed = -speed
 
 func _physics_process(delta):
 	global_position.x += current_speed * delta
 	current_speed += acceleration
-	current_speed = clampf(current_speed, 0, speed)
+	current_speed = clampf(current_speed, -speed, speed)
