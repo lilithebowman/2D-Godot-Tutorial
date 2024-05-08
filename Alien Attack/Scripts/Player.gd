455785f1c8f43f46c1e794c136d018805c4105ea
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-05-08 13:19:07 -0400

    Clamp the player's position to the viewport

--- Alien Attack/Scripts/Player.gd
+++ Alien Attack/Scripts/Player.gd
@@ -1,11 +1,20 @@
-extends Node
+extends CharacterBody2D
 
+var speed = 300
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	velocity = Vector2(0, 0)
+	
+	if Input.is_action_pressed("Up"):
+		velocity.y = -speed
+	elif Input.is_action_pressed("Down"):
+		velocity.y = speed
+	
+	if Input.is_action_pressed("Left"):
+		velocity.x = -speed
+	elif Input.is_action_pressed("Right"):
+		velocity.x = speed
+	move_and_slide()
+	
+	global_position = global_position.clamp(Vector2(0, 0), get_viewport_rect().size)
 	pass
