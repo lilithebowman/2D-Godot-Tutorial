4b41b4ab951b9b1a385c79ba3712b32fff11ecdc
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-05-09 10:29:22 -0400

    Rockets queue_free() themselves when offscreen

--- Alien Attack/Scripts/rocket.gd
+++ Alien Attack/Scripts/rocket.gd
@@ -2,9 +2,18 @@ extends Area2D
 
 @export var acceleration = 25
 @export var speed = 300
+@onready var visibleOnScreenNotifier = $VisibleOnScreenNotifier2D
 var current_speed = -(speed / 2)
+
+func _ready():
+	visibleOnScreenNotifier.connect("screen_exited", _on_screen_exited)
 
 func _physics_process(delta):
 	global_position.x += current_speed * delta
 	current_speed += acceleration
 	current_speed = clampf(current_speed, -speed, speed)
+
+func _on_screen_exited():
+	print("Rocket exited screen")
+	queue_free()
+	pass
