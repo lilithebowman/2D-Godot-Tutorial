1d5ed099f655d010d2203282f6732c4c2c2e74f4
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-05-14 22:33:49 -0400

    spawner

--- Alien Attack/Scripts/enemy_spawner.gd
+++ Alien Attack/Scripts/enemy_spawner.gd
@@ -1,4 +1,16 @@
 extends Node2D
 
+var enemy_scene = preload("res://Scenes/Enemy.tscn")
+
+@onready var spawn_positions = $SpawnPositions
+
 func _on_timer_timeout():
+	spawn_enemy()
 	print("Timed out")
+
+func spawn_enemy():
+	var spawn_positions_array = spawn_positions.get_children()
+	var enemy_instance = enemy_scene.instantiate()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	enemy_instance.global_position = random_spawn_position.global_position
+	add_child(enemy_instance)
