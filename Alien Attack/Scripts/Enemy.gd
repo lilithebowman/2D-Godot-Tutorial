a2f6377f3789cb711884ab759c50f709191b2efc
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-05-16 16:00:14 -0400

    Enemies now Area2D and they take damage

--- Alien Attack/Scripts/Enemy.gd
+++ Alien Attack/Scripts/Enemy.gd
@@ -1,7 +1,14 @@
-extends CharacterBody2D
+extends Area2D
 
 @export var speed = 300
+@export var health = 100
 
 func _physics_process(delta):
-	velocity.x = -speed
 	global_position.x += -speed * delta
+	
+func take_damage(damage):
+	health -= damage
+	if health <= 0:
+		queue_free()
+		print("Enemy health depleted; enemy destroyed!")
+	
