258236fb56d6eabc37f9a081796d48aefc6cbf52
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-05-16 16:17:42 -0400

    Destroy enemies off the left of the screen

---
+++ Alien Attack/Scripts/game.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_enemy_death_zone_area_entered(area):
+	print(area.name)
+	area.take_damage(100)
+	pass # Replace with function body.
