0b12e2d8f75e30999fac66f5551ae03a831546d2
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-12-04 10:49:41 -0500

    Hooked up animations for test

---
+++ martian-mike/scripts/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _process(delta):
+	if Input.is_action_pressed("move_left"):
+		animated_sprite.play("Run")
+	elif Input.is_action_pressed("move_right"):
+		animated_sprite.play("Run")
+	elif Input.is_action_pressed("jump"):
+		animated_sprite.play("Jump")
+	else:
+		animated_sprite.play("Idle")
+
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+#
+#
+#func _physics_process(delta: float) -> void:
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
