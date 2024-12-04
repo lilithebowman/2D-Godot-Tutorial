9318d971b3a03f0cd3d7e575666a0871721c0993
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-12-04 11:16:25 -0500

    Hooked up all of the animations

--- martian-mike/scripts/player.gd
+++ martian-mike/scripts/player.gd
@@ -2,15 +2,46 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(delta):
-	if Input.is_action_pressed("move_left"):
+@export var gravity = 400
+@export var jump_force = 200
+@export var speed = 125
+
+func _physics_process(delta: float) -> void:
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+		if velocity.y > 500:
+			velocity.y = 500 # clamp it at 500
+
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	
+	# animations
+	if velocity.x != 0 && is_on_floor():
 		animated_sprite.play("Run")
-	elif Input.is_action_pressed("move_right"):
-		animated_sprite.play("Run")
-	elif Input.is_action_pressed("jump"):
-		animated_sprite.play("Jump")
 	else:
-		animated_sprite.play("Idle")
+		animated_sprite.play("Idle") # default to Idle animation
+
+	if velocity.y > 0:
+		animated_sprite.play("Fall")
+	elif velocity.y < 0:
+		animated_sprite.play("Jump")
+		
+	move_and_slide()
+
+
+
+#func _process(delta):
+	#if Input.is_action_pressed("move_left"):
+		#animated_sprite.play("Run")
+	#elif Input.is_action_pressed("move_right"):
+		#animated_sprite.play("Run")
+	#elif Input.is_action_pressed("jump"):
+		#animated_sprite.play("Jump")
+	#else:
+		#animated_sprite.play("Idle")
 
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
